271efa753a657f1cfe0b4f3341716bf5720eb53a
Nikita1320 <105381734+Nikita1320@users.noreply.github.com>
2024-08-12 19:02:08 +0300

    Init (added ServiceProject, ModelProject)

---
+++ Services/ScheduleService.cs
@@ -0,0 +1,30 @@
+﻿namespace Services
+{
+    public class ScheduleService
+    {
+        public List<Models.Task> GetDayTask(DateTime date)
+        {
+            return null;
+        }
+        public List<Models.Task> GetNotCompletedTasks(DateTime date)
+        {
+            return null;
+        }
+        public List<Models.Task> GetCompletedTasks(DateTime date)
+        {
+            return null;
+        }
+        public List<Models.Task> GetTasks(DateTime startTime, DateTime endTime)
+        {
+            return null;
+        }
+        public List<Models.Task> GetNotCompletedTasks(DateTime startTime, DateTime endTime)
+        {
+            return null;
+        }
+        public List<Models.Task> GetCompletedTasks(DateTime startTime, DateTime endTime)
+        {
+            return null;
+        }
+    }
+}
